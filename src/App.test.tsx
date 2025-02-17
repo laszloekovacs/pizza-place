02f8d714b30a98cb34256e124d194bf47192f14c
laszloekovacs <laszloekovacs@gmail.com>
2025-02-17 14:36:59 +0100

    initial implementation

---
+++ src/App.test.tsx
@@ -0,0 +1,24 @@
+import { render, screen, within } from '@testing-library/react'
+import '@testing-library/jest-dom/vitest'
+import { describe, expect, it } from 'vitest'
+import App from './App'
+
+function PizzaShopApp() {
+	return <>The Code Oven</>
+}
+
+describe('The Application', () => {
+	it('renders application heading', () => {
+		render(<PizzaShopApp />)
+		const heading = screen.getByText('The Code Oven')
+		expect(heading).toBeInTheDocument()
+	})
+
+	it('renders menu list', () => {
+		render(<PizzaShopApp />)
+		const menuList = screen.getByRole('list')
+		const menuItems = within(menuList).getAllByRole('listitem')
+
+		expect(menuItems.length).toEqual(8)
+	})
+})
